9705062cef846b4ea596782dcc32d2c89ddab9b5
Ioannis Karamanlakis <john.karamanlakis@gmail.com>
2013-10-06 17:08:06 +0100

    added a loop

--- Hello-World/src/HelloWorld.java
+++ Hello-World/src/HelloWorld.java
@@ -1,10 +1,14 @@
 /**
  * Created with IntelliJ IDEA.
+ *
  * @author yannis
  */
 public class HelloWorld {
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
+        for (int i = 0; i <= 3; i++) {
+            System.out.println(i);
+        }
         System.out.println("Hello world!");
     }
 
