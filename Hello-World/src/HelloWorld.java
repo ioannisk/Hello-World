4cf142bd8fa016713223966509f61910afbf4a61
Ioannis Karamanlakis <john.karamanlakis@gmail.com>
2013-10-06 16:53:46 +0100

    added HelloWorld class

---
+++ Hello-World/src/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * Created with IntelliJ IDEA.
+ * @author yannis
+ */
+public class HelloWorld {
+
+    public static void main(String args[]){
+        System.out.println("Hello world!");
+    }
+
+}
